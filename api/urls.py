2a568b9e0c407796f3c6257b07bdc2add5161d79
djumanov <djumanovdev@gmail.com>
2026-03-07 09:02:20 +0500

    init

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import HelloWorldView
+
+
+urlpatterns = [
+    path('hello/', HelloWorldView.as_view(), name='hello-world'),
+]
---
+++ api/views.py
@@ -0,0 +1,8 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({'message': 'Hello, World!'})
+    
